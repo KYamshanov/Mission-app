dd34f467d9dfd3e5e0dfec627e7e97f4548cfe50
Константин Ямшанов <yamshanov@dev.vtb.ru>
2023-06-24 14:15:58 +0400

    Вынесение настроек гредла модулей в плагины в buildSrc

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
-    alias(libs.plugins.multiplatform).apply(false)
-    alias(libs.plugins.compose).apply(false)
-    alias(libs.plugins.android.application).apply(false)
+    use(libs.plugins.multiplatform).apply(false)
+    use(libs.plugins.compose).apply(false)
+    use(libs.plugins.android.application).apply(false)
+
     alias(libs.plugins.kotlinx.serialization).apply(false)
 }
