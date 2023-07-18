7870e06941aa376e24658e003b56399bb97b7032
Константин Ямшанов <yamshanov@dev.vtb.ru>
2023-07-18 10:51:24 +0400

    Интеграция JS

---
+++ js-test/build.gradle.kts
@@ -0,0 +1,40 @@
+import org.gradle.accessors.dm.LibrariesForLibs
+
+//see https://github.com/gradle/gradle/issues/15383
+val libs = the<LibrariesForLibs>()
+
+plugins {
+    id("org.jetbrains.kotlin.multiplatform")
+    id("org.jetbrains.compose")
+
+}
+
+
+kotlin {
+    js(IR) {
+        browser()
+        binaries.executable()
+    }
+
+    sourceSets {
+        val jsMain by getting {
+            dependencies {
+                implementation(compose.html.core)
+                implementation(compose.runtime)
+            }
+        }
+    }
+}
+/*
+// workaround for https://youtrack.jetbrains.com/issue/KT-48273
+afterEvaluate {
+    rootProject.extensions.configure<org.jetbrains.kotlin.gradle.targets.js.nodejs.NodeJsRootExtension> {
+        versions.webpackDevServer.version = "4.0.0"
+        versions.webpackCli.version = "4.10.0"
+    }
+}*/
+/*
+compose {
+    kotlinCompilerPlugin.set(Versions.jbComposeCompiler)
+    kotlinCompilerPluginArgs.add("suppressKotlinVersionCompatibilityCheck=1.9.0")
+}*/
---
+++ js-test/src/jsMain/kotlin/Main.kt
@@ -0,0 +1,54 @@
+import androidx.compose.runtime.*
+import kotlinx.coroutines.InternalCoroutinesApi
+import org.jetbrains.compose.web.attributes.InputType
+import org.jetbrains.compose.web.css.*
+import org.jetbrains.compose.web.dom.*
+import org.jetbrains.compose.web.renderComposable
+
+//Development based on the PeopleInSpace project / https://github.com/joreilly/PeopleInSpace
+
+@InternalCoroutinesApi
+fun main() {
+    renderComposable(rootElementId = "root") {
+        Style(TextStyles)
+
+
+
+        Div(attrs = { style { padding(16.px) } }) {
+            H1(attrs = { classes(TextStyles.titleText) }) {
+                Text("Mission-app")
+            }
+        }
+    }
+}
+
+object TextStyles : StyleSheet() {
+
+    val titleText by style {
+        color(rgb(23,24, 28))
+        fontSize(50.px)
+        property("font-size", 50.px)
+        property("letter-spacing", (-1.5).px)
+        property("font-weight", 900)
+        property("line-height", 58.px)
+
+        property(
+            "font-family",
+            "Gotham SSm A,Gotham SSm B,system-ui,-apple-system,BlinkMacSystemFont,Segoe UI,Roboto,Oxygen,Ubuntu,Cantarell,Droid Sans,Helvetica Neue,Arial,sans-serif"
+        )
+    }
+
+    val personText by style {
+        color(rgb(23,24, 28))
+        fontSize(24.px)
+        property("font-size", 28.px)
+        property("letter-spacing", "normal")
+        property("font-weight", 300)
+        property("line-height", 40.px)
+
+        property(
+            "font-family",
+            "Gotham SSm A,Gotham SSm B,system-ui,-apple-system,BlinkMacSystemFont,Segoe UI,Roboto,Oxygen,Ubuntu,Cantarell,Droid Sans,Helvetica Neue,Arial,sans-serif"
+        )
+    }
+}
